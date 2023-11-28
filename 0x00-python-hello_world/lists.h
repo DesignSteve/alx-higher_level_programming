5437b69ca83072965287b2aeb9710fc2c3e144ce
DesignSteve <steveboss008@gmail.com>
2023-11-27 21:00:19 -0800

    update

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,29 +1,8 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 
-/**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
- */
-int check_cycle(listint_t *list)
-{
-        listint_t *slow = list;
-        listint_t *fast = list;
+#include <stdlib.h>
 
-        if (!list)
-                return (0);
-
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
-
-        return (0);
-}
 /**
  * struct listint_s - singly linked list
  * @n: integer
