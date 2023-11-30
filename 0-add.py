67785c9cae1a98238b58911989e45fbd778ddb14
DesignSteve <steveboss008@gmail.com>
2023-11-30 12:29:53 -0800

    update

---
+++ 0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add(a, b):
+    return a + b
+
+# main_program.py
+a = 1
+b = 2
+
+from add_0 import add
+
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
+
+
